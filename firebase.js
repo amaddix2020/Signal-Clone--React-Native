394f07f048f2f27e70db4330dbb4b33e9d510e57
Adrian <amaddix2012@gmail.com>
2021-01-30 19:42:16 +0000

    created firebase folder

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBgkz7O-zep48GAR9BKpWRNB_QuxbMXlDI",
+    authDomain: "signal-clone-9a800.firebaseapp.com",
+    projectId: "signal-clone-9a800",
+    storageBucket: "signal-clone-9a800.appspot.com",
+    messagingSenderId: "1064571974352",
+    appId: "1:1064571974352:web:e1ec12ef9af3e3ef4364d5"
+  };
