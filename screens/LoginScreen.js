ec2efa6cea50da5c1a1fd94610c1cc1bacead967
Adrian <amaddix2012@gmail.com>
2021-01-30 21:33:42 +0000

    added signal image to LoginScreen.js

---
+++ screens/LoginScreen.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import { Button, Input, Image } from "react-native-elements";
+import { StatusBar } from 'expo-status-bar';
+
+const LoginScreen = () => {
+    return (
+        <View>
+            <StatusBar style="light" />
+          
+          <Image source={{
+              uri:"https://blog.mozilla.org/internetcitizen/files/2018/08/signal-logo.png",
+          }} 
+          style= {{ width:200, height: 200 }} 
+          />
+          <View styles={styles.inputContainer}>
+              <Input />
+
+          </View>
+        </View>
+        
+    );
+};
+
+export default LoginScreen
+
+const styles = StyleSheet.create({
+    inputContainer: {
+
+    },
+});
