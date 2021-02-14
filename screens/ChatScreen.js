09646f391dcf8a4d638e060aa54844f46ebd6bb5
Adrian <amaddix2012@gmail.com>
2021-02-14 18:46:06 +0000

    updated files

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native'
 const ChatScreen = () => {
     return (
         <View>
-            <Text></Text>
+            <Text>chat Screen</Text>
         </View>
     )
 }
